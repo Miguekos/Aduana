2ced9d0d1de472f2b28e5b6416ec4c7283234b29
Miguekos <Alexkos12>
2018-06-30 22:56:26 -0500

    fix

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,11 +14,12 @@ class DatabaseSeeder extends Seeder
 
         $this->call(UserTableSeeder::class);
         $this->call(FrecuenciaTableSeeder::class);
-        $this->call (HorarioTableSeeder::class);
-        $this->call (ModalidadTableSeeder::class);
-        $this->call (PagoTableSeeder::class);
-        $this->call (TipodecursoTableSeeder::class);
-        $this->call (PublicidadTableSeeder::class);
+        $this->call(HorarioTableSeeder::class);
+        $this->call(ModalidadTableSeeder::class);
+        $this->call(PagoTableSeeder::class);
+        $this->call(TipodecursoTableSeeder::class);
+        $this->call(PublicidadTableSeeder::class);
+        $this->call(PruebaTableSeeder::class);
         // $this->call(MembresiaTableSeeder::class);
 
     }
